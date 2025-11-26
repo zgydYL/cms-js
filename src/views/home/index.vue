6c9ee70e7068e5a8f6d5115e45f6e19fd2e73c34
zgyd <cjun7627@gmail.com>
2025-11-27 00:38:48 +0800

    更新 favicon.ico

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="homePage">
     <h1>homePage</h1>
+    <ul>
+      <li v-for="value in 10">{{ value }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup>
+
+
+
 </script>
 
 <style scoped>
